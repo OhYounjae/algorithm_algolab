9a4aae0c9171369a279b837cd8c6d2f03d122415
Younjae Oh <younjae@kookmin.ac.kr>
2019-09-26 16:24:36 +0900

    solving yet

--- 20190928_palindrome.cpp
+++ 20190928_palindrome.cpp
@@ -4,8 +4,21 @@
 
 using namespace std;
 
-void getParlindrom(string str){
-    
+void changeNum(int num){ //진법 변환
+    string str;
+    //if(num %)
+
+    isPalindrome(str);
+}
+
+void isPalindrome(string str){ // 팰린드롬인지 확인
+    for(int i = 0; i < str.length(); i++){
+        if(str[i] != str[str.length()-i]){
+            cout << 0 << endl;
+            return;
+        }
+    }
+    cout << 1 << endl;
 }
 
 int main(){
@@ -14,11 +27,21 @@ int main(){
 
     int t;
     in >> t;
-
     
     while(t--){
-        string str;
-        in >> str;
+        int num;
+        in >> num;
+        string str = to_string(num);
 
+        //10진수 먼저 판별
+        for(int i = 0; i < str.length(); i++){
+            if(str[i] != str[str.length()-1-i]){
+                changeNum(num);
+                break;
+            }
+        }
+        
     }
+
+    in.close();
 }
