e3da085ebbd89fe8518132d28106b48b6d35a805
Younjae Oh <younjae@kookmin.ac.kr>
2019-10-01 12:19:55 +0900

    solved

--- 20191002_truck.cpp
+++ 20191002_truck.cpp
@@ -5,8 +5,7 @@
 
 using namespace std;
 
-vector<int> truck; 
-queue<int> car;
+queue<int> truck;
 
 int main(){
     ifstream in;
@@ -18,21 +17,56 @@ int main(){
         int n, w, L;
         in >> n >> w >> L;
         //n: 트럭의 수, w: 다리의 길이, L: 최대하중
-        
-        int weight = 0;
+
+        int n_w = w - 1; //배열 bridge의 최대 인덱스 값
+        int bridge[w];
+        for(int i = 0; i < w; i++){
+            bridge[i] = 0;
+        }
+
+        int weight = 0;//현재 무게
+        int cnt = 1;
         while (n--){ //큐에 각 트럭의 무게 입력
             int temp;
             in >> temp;
-            truck.push_back(temp);
+            truck.push(temp);
         }
         
-        for(int i = 0; i < truck.size(); i++){
-            int temp;
-            if(weight = 0){
-                temp = truck[i];
-                car.push(temp);
-                weight += temp;
+        while(!truck.empty()){
+            if(weight + truck.front() <= L){ // 다리위에 차 올려놓는 것
+                bridge[n_w] = truck.front();
+                weight += truck.front();
+                truck.pop();
             }
+            // 움직여
+            weight -= bridge[0];
+            for(int i = 0; i < n_w; i++){
+                bridge[i] = bridge[i+1];
+            }
+            bridge[n_w] = 0;
+
+            // 초 += 1
+            cnt += 1;
         }
+        // cout << weight << endl;
+
+        while(weight != 0){//마지막으로 들어간 트럭, 다리 건너게 해주기
+            weight -= bridge[0];
+            for(int i = 0; i < n_w; i++){
+                bridge[i] = bridge[i+1];
+                if (bridge[i] != 0){
+                    //cout << bridge[i] << " " << i << endl;
+                    //cout << weight << endl;
+                }
+            }
+            bridge[n_w] = 0;
+            
+            // 초 += 1
+            cnt += 1;
+        }
+        cout << cnt << endl;
+        
     }
+
+    in.close();
 }
